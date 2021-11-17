6ed78605f8d36550768a3c73639c3cbb98c8c9f6
Lins <294127195@qq.com>
2021-11-18 00:13:29 +0800

    接口自动化练习

--- api_tests/test_user.py
+++ api_tests/test_user.py
@@ -6,6 +6,7 @@ import json
 import logging
 import time
 
+import pystache
 import requests
 
 from api_tests.test_token import Weixin
@@ -34,16 +35,30 @@ class TestUser:
         print(r)
         assert r["errcode"] == 0
 
-    # 模板创建
-    def test_create_tem(self):
-        data = {
+    # 模板创建，模板替换
+    def test_create_template(self):
+        print(pystache.render("hello {{name}} {{#has}} world {{value}}  {{/has}}",
+                              {"name": "lins",
+                               "has": [{"value": 1}, {"value": 2}, {"value": 3}],
+                               }))
 
-        }
+    def test_create_by_template(self):
+        uid = "seveniruby_" + str(time.time())
+        mobile = str(time.time()).replace(".", "")[0:11]
+        data = str(self.get_user({
+            "name": uid,
+            "title": "校长",
+            "email": "2222@qq.com",
+            "mobile": mobile
+        }))
+        data = data.encode("UTF-8")
+
         r = requests.post("https://qyapi.weixin.qq.com/cgi-bin/user/create",
                           params={"access_token": Weixin.get_token()},
-                          json=data).json()
+                          data=data,
+                          headers={"content-tyoe": "application/json;charset=UTF-8"}
+                          ).json()
         logging.debug(r)
-        print(r)
         assert r["errcode"] == 0
 
     def test_list(self):
@@ -55,3 +70,11 @@ class TestUser:
         # print(r)
 
         # assert r["errcode"]==0
+
+    def get_user(self, dict):
+        template = "".join(open("user_create.json", encoding='utf-8').readlines())
+        logging.debug(template)
+        return pystache.render(template, dict)
+
+    def test_get_user(self):
+        logging.debug(self.get_user({"name": "seveniruby", "title": "校长", "email": "1@.com"}))
--- api_tests/user.py
+++ api_tests/user.py
@@ -2,3 +2,22 @@
 # File_Name:user
 # -*- coding:utf-8 -*-
 # Author:Lins_369
+import requests
+
+from api_tests.token import Weixin
+
+
+class User:
+    def create(self, dict=None, data=None):
+        return requests.post("https://qyapi.weixin.qq.com/cgi-bin/user/create",
+                             params={"access_token": Weixin.get_token()},
+                             json=dict,
+                             ).json()
+
+    def list(self, department_id=23, fetch_child=0, **kwargs):
+        return requests.get("https://qyapi.weixin.qq.com/cgi-bin/user/simplelist",
+                            params={"access_token": Weixin.get_token(),
+                                    "department_id": department_id,
+                                    "fetch_child": fetch_child
+                                    }
+                            ).json()
